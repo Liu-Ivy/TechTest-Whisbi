7e68916593ba8a3461a9ee4efce59d521f64d8a9
Ivy Liu <ivyliu402@gmail.com>
2020-05-23 09:00:26 +0200

    show result on html done

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
-
-// Create Express app
 const app = express();
+const path = require("path");
+const router = express.Router();
 
 // A sample route
-app.get("/", (req, res) => res.send("Hello World!"));
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
 
 // Start the Express server
+app.use("/", router);
 app.listen(3000, () => console.log("Server running on port 3000!"));
