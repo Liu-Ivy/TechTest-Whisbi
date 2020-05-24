0d61148f0c35fffff4afa74460fa8ad0cfa98be4
Ivy Liu <ivyliu402@gmail.com>
2020-05-24 16:20:37 +0200

    last checking

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const app = express();
 const path = require("path");
 const router = express.Router();
 
-// A sample route
 router.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-// Start the Express server
 app.use(express.static(__dirname + "/public"));
 app.use("/", router);
 app.listen(3000, () => console.log("Server running on port 3000!"));
